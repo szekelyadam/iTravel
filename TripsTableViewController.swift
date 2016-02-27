40cb4eede74aeae2c7f02171180883246a899346
szekelyadam <szekelyadam@sch.bme.hu>
2016-02-27 21:30:33 +0100

    my trips in dynamic table view

---
+++ TripsTableViewController.swift
@@ -0,0 +1,91 @@
+//
+//  TripsTableViewController.swift
+//  iTravel
+//
+//  Created by Ádibádi on 27/02/16.
+//  Copyright © 2016 Székely Ádám. All rights reserved.
+//
+
+import UIKit
+
+class TripsTableViewController: UITableViewController {
+    
+    var tripsDataManager : TripsDataManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tripsDataManager = AppDelegate.sharedAppDelegate().tripsDataManager
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tripsDataManager!.trips.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("TripsTableViewCell")! as UITableViewCell
+        
+        let tripData = tripsDataManager!.trips[indexPath.row] as! [String:AnyObject]
+        cell.textLabel?.text = tripData["name"] as? String
+        cell.detailTextLabel?.text = tripData["location"] as? String
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
