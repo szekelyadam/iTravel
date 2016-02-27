eab299fcc441af71b7a2e7f7437f57e599b47724
szekelyadam <szekelyadam@sch.bme.hu>
2016-02-27 20:52:04 +0100

    Initial Commit

---
+++ iTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTravel
+//
+//  Created by Ádibádi on 27/02/16.
+//  Copyright © 2016 Székely Ádám. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
